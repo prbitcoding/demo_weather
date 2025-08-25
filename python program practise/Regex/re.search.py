9e4fce656b6b5ee36f3559878eee0b1d32ca40a7
rajani <rajaniharendra58@gmail.com>
2025-08-25 18:57:17 +0530

    update

--- python program practise/Regex/re.search.py
+++ python program practise/Regex/re.search.py
@@ -1,25 +1,27 @@
 import re
 
-pattern = r"[A-Z]ind"
+pattern = "python"
 
 text = '''
 Please use the search box at the top of this page or the links to the right. Feel free to subscribe to our syndicated feeds.
 Using?
 To fulfill the free license requirements, please read our  Rind TindReuse guide. You can also request a file.
 Identifying?
-Have a browse through Category:Unidentified subjects. If you Find something you can identify, adjust the file's categories and depicts statements in structured data.
+Have a browse through  please Category:Unidentified subjects. If you Find something you can identify, adjust the file's categories and depicts statements in structured data.
 Creating?
-Check out all you need to know at our Contributing your own work guide.
+Check out all you need to know at our Contributing your own work guide.please
 and more!
 Discussion?
 See the list of discussion pages.
 To explore more ways you can contribute to this project, Cind Kind check out the Community Portal.'''
 
-# match = re.search(pattern, text)
-# print(match)
+match = re.search(pattern, text, re.IGNORECASE)
+print(match)
+print(type(match))
 
-matches = re.finditer(pattern, text)
- 
-for match in matches:
-    print(match.span())
-    print(text[match.span()[0]: match.span()[1]])
+if match:
+    print("found:",match.group())
+else:
+    print("Not found")
+
+  
