5a08f9d4b68ff2702d1dc954d851e97cdccf75e1
rajani <rajaniharendra58@gmail.com>
2025-10-03 14:56:43 +0530

    todo list

---
+++ taskmanag.py/app.py
@@ -0,0 +1,25 @@
+def task():
+    tasks = []
+    print("-----WELCOME TO THE TASK MANAGEMENT APP-----")
+
+    total_task = int(input("Enter how many task you want to add =  "))
+    for i in range(1, total_task+1):
+        task_name = input(f"Enter task {i} = ")
+        tasks.append(task_name)
+
+    print(f"Today's tasks are\n{tasks}")
+
+    while True:
+        operation = int(input("Enter 1-Add\n2-Update\n3-Delete\n4-View\n5-Exit/stop/"))
+        if operation == 1:
+            add = input("enter task you want to add = ")
+            tasks.append(add)
+            print(f"Task {add} has been suceesfully added...")
+
+        elif operation == 2:
+            updated_val = input("Enter the task name you want to update = ")
+            if updated_val in tasks:
+                up = input("Enter new task = ")
+                ind = task.index(updated_val)
+                tasks[ind] = up
+                print(f"Updated task {up}")
