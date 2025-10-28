f91449df4032b9f28bfa5108111199f9bcce95ec
rajani <rajaniharendra58@gmail.com>
2025-10-28 18:31:52 +0530

    add function

--- javascript/08_function.js
+++ javascript/08_function.js
@@ -1,34 +1,44 @@
-var a = 5, 
-    b = 10;
+// var a = 5, 
+//     b = 10;
 
-var sum1 = a + b;
-console.log(sum1)
-
-
-// 2nd student
-
-var a = 35, 
-    b = 50;
-
-var sum2 = a + b;
-console.log(sum2)
-
-// 3rd student
-
-var a = 15, 
-    b = 20;
-
-var sum3 = a + b;
-console.log(sum3)
+// var sum1 = a + b;
+// console.log(sum1)
 
 
-// lets make a reusable code
+// // 2nd student
 
-function sum(a, b){
+// var a = 35, 
+//     b = 50;
+
+// var sum2 = a + b;
+// console.log(sum2)
+
+// // 3rd student
+
+// var a = 15, 
+//     b = 20;
+
+// var sum3 = a + b;
+// console.log(sum3)
+
+
+// // lets make a reusable code
+
+var result  = (function (a, b){
+    console.log(a + b);
     return a + b;
-}
+})(5, 5);
 
-console.log(sum(5, 5));
-console.log(sum(15, 15));
-console.log(sum(25, 25));
-console.log(sum(35, 35));
+console.log("the sum of the two number is " + result)
+
+
+ 
+
+// function greet(name) {
+//     console.log("Hello " + name + ", welcome to thapa technical js course");
+// }
+
+// greet("vinod");
+// greet("Harii");
+
+ 
